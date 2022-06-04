00cf908399440cb33bbbb1ed04b1c0aaa4187abe
RestuAlamBagaskara <alambagaskara18@gmail.com>
2022-06-04 18:24:28 +0700

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::paginate(5);
+        return response()->json([
+            'success' => true,
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|url',
+            'category_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->all()
+            ], 422);
+        }
+
+        $request['user_id'] = Auth::user()->id;
+
+        $article = Article::create($request->toArray());
+
+        if ($article)
+            return response()->json([
+                'success' => true,
+                'article' => $article
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Article not added'
+            ], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $articles = Article::find($id);
+
+        if (!$articles) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Article not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'article' => $articles
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|url',
+            'category_id' => 'required',
+            'user_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->all()
+            ], 422);
+        }
+
+        $article = Article::where('id', $id);
+
+        if (!$article) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Article not found'
+            ], 400);
+        }
+
+        $updated = Article::where('id', $id)->update($request->toArray());
+
+        if ($updated)
+            return response()->json([
+                'success' => true,
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Article can not be updated'
+            ], 500);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::where('id', $id);
+
+        if (!$article) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Article not found'
+            ], 400);
+        }
+
+        if (Article::destroy($id)) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Article can not be deleted'
+            ], 500);
+        }
+    }
+}
